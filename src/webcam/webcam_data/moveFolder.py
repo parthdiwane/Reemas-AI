ac0ce5afc16c2cf8151412ddfbedde5d3159ae65
parthdiwane <parthdiwane@gmail.com>
2024-09-05 10:57:05 -0700

    updated the repo

--- src/webcam/webcam_data/moveFolder.py
+++ src/webcam/webcam_data/moveFolder.py
@@ -5,6 +5,6 @@ images = [f for f in os.listdir() if '.png' in f.lower()]
 
 
 for images in images:
-    new_path = 'src/webcam/storedImgs'
+    new_path = 'src/assets/storedImgs'
     shutil.move(images, new_path)
     
