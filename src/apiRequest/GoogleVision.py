75b3251b45ce90c97df44e7961d1f11250d1eae3
parthdiwane <parthdiwane@gmail.com>
2024-09-06 12:36:36 -0700

    added the vison api for ocr

--- src/apiRequest/GoogleVision.py
+++ src/apiRequest/GoogleVision.py
@@ -1,23 +1,22 @@
 from google.cloud import vision
-import os
+import os 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '814c4dbd41c403783ba8f7e8c0b221d3b785db24.json'
-
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "src/apiRequest/creds.json"
 def detect_text(path):
-    """Detects text in the given image."""
+    """Detects text in the file."""
     client = vision.ImageAnnotatorClient()
-
     with open(path, 'rb') as image_file:
         content = image_file.read()
-
     image = vision.Image(content=content)
-
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    if len(texts) > 0:
-        print(texts[0].description)
-    else:
-        print('No text detected.')
+    print('Texts:')
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+        vertices = [f'({vertex.x},{vertex.y})' for vertex in text.bounding_poly.vertices]
+        print('bounds: {}'.format(','.join(vertices)))
+    if response.error.message:
+        raise Exception(f'{response.error.message}\nFor more info on error messages, check: https://cloud.google.com/apis/design/errors')
 
-image_path = 'image.png'
-detect_text(image_path)
+# Replace 'PATH_TO_YOUR_IMAGE' with the path to the image file you want to analyze.
+detect_text('src/assets/imgs/test.png')
