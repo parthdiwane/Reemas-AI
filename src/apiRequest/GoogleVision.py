079c56a55059093ccf7ef45aa04b52379ae847ef
sameerindrayan <sameer.indrayan07@gmail.com>
2024-09-06 13:54:02 -0700

    combines individual strings into sentences

--- src/apiRequest/GoogleVision.py
+++ src/apiRequest/GoogleVision.py
@@ -10,13 +10,22 @@ def detect_text(path):
     image = vision.Image(content=content)
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-        vertices = [f'({vertex.x},{vertex.y})' for vertex in text.bounding_poly.vertices]
-        print('bounds: {}'.format(','.join(vertices)))
+    
+    full_text = "" #starts as empty string to store all text
+    
+    if texts:
+        for text in texts:
+            full_text += text.description + " " #put space between each word
+            print(full_text.strip()) #print full text, while removing any trailing space
+    else:
+        print("No text found in the image.")
+    # print('Texts:')
+    # for text in texts:
+    #     print('\n"{}"'.format(text.description))
+    #     vertices = [f'({vertex.x},{vertex.y})' for vertex in text.bounding_poly.vertices]
+    #     #print('bounds: {}'.format(','.join(vertices)))
     if response.error.message:
         raise Exception(f'{response.error.message}\nFor more info on error messages, check: https://cloud.google.com/apis/design/errors')
 
 # Replace 'PATH_TO_YOUR_IMAGE' with the path to the image file you want to analyze.
-detect_text('src/assets/imgs/test.png')
+detect_text('src/assets/imgs/testsentences.png')
