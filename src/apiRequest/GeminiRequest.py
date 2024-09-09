68d8782684e45259831014d62f923c02f193e7cc
parthdiwane <parthdiwane@gmail.com>
2024-09-09 12:09:28 -0700

    added color to the terminal for ease of output

--- src/apiRequest/GeminiRequest.py
+++ src/apiRequest/GeminiRequest.py
@@ -1,6 +1,11 @@
 import os
 import google.generativeai as genai
 from GoogleVision import *
+import colorama
+from colorama import Fore, Style
+
+colorama.init()
+
 genai.configure(api_key=os.environ['API_KEY_GEMINI'])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
@@ -15,16 +20,17 @@ chat = model.start_chat(
 
 img_path = 'src/assets/imgs/obama.png'
 
-print('hello, please state the name of the person talking and any other nessassry context: ')
+print(Fore.BLUE + 'hello, please state the name of the person talking and any other nessassry context: ')
 context = input()
 
 txt_from_img = detect_text(img_path)
 print("\n")
-print('verifying: ' + detect_text(img_path))
+print(Fore.RED + 'verifying: ' + detect_text(img_path))
 
 print('\n')
 responce = chat.send_message("context: " + context + " verify the following fact: " + txt_from_img + " . State only whether if the fact is true, false, unsure, or not enough information provided. Do not explain why the fact is true or false. Please keep you responces limited to the four options gi")
-print(responce.text)
+print(Fore.GREEN + "Output: " +responce.text)
+print(Style.RESET_ALL)
 
 
 
