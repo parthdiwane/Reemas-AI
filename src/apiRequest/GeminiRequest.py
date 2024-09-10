f853829b0d921dd525edecf1eedc88f7e78c5908
sameerindrayan <sameer.indrayan07@gmail.com>
2024-09-09 21:14:28 -0700

    idk why this file got deleted

--- src/apiRequest/GeminiRequest.py
+++ src/apiRequest/GeminiRequest.py
@@ -18,19 +18,18 @@ chat = model.start_chat(
     ]
 )
 
-img_path = 'src/assets/imgs/obama.png'
+img_path = 'src/assets/imgs/false.png'
 
-print(Fore.BLUE + 'hello, please state the name of the person talking and any other nessassry context: ')
+print('hello, please state the name of the person talking and any other nessassry context: ')
 context = input()
 
 txt_from_img = detect_text(img_path)
 print("\n")
-print(Fore.RED + 'verifying: ' + detect_text(img_path))
+print( 'verifying: ' + detect_text(img_path))
 
 print('\n')
 responce = chat.send_message("context: " + context + " verify the following fact: " + txt_from_img + " . State only whether if the fact is true, false, unsure, or not enough information provided. Do not explain why the fact is true or false. Please keep you responces limited to the four options gi")
-print(Fore.GREEN + "Output: " +responce.text)
-print(Style.RESET_ALL)
+print( + "Output: " +responce.text)
 
 
 
