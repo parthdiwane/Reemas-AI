8f62a8ff6ba85ca826d1e4c8fbd8f06dd88c4c7c
parthdiwane <parthdiwane@gmail.com>
2024-09-10 11:59:56 -0700

    added messages and fixed the issue where it says google module not found

--- src/apiRequest/GeminiRequest.py
+++ src/apiRequest/GeminiRequest.py
@@ -20,16 +20,17 @@ chat = model.start_chat(
 
 img_path = 'src/assets/imgs/false.png'
 
-print('hello, please state the name of the person talking and any other nessassry context: ')
+print(Fore.BLUE + 'hello, please state the name of the person talking and any other nessassry context: ')
 context = input()
 
 txt_from_img = detect_text(img_path)
 print("\n")
-print( 'verifying: ' + detect_text(img_path))
+print(Fore.GREEN + 'verifying: ' + detect_text(img_path))
 
 print('\n')
 responce = chat.send_message("context: " + context + " verify the following fact: " + txt_from_img + " . State only whether if the fact is true, false, unsure, or not enough information provided. Do not explain why the fact is true or false. Please keep you responces limited to the four options gi")
-print("Output: " +responce.text)
+print(Fore.RED + "Output: " +responce.text)
+print(Fore.RESET)
 
 
 
